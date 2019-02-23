0232a43db21df12d67b360542e501742d0de64b0
Abhishek Singh <33527786+abhisheksingh98@users.noreply.github.com>
2019-02-23 23:45:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ require('./startup/config')();
 require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => winston.info(`Listening on port ${port}...`));
+app.listen(port, () => winston.info(`Listening on port ${port}...`))
